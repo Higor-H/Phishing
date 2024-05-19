61a6a4839eeb2ea258121a391f4f791fc031c819
Higor Milani <milanihigor016@gmail.com>
2024-05-19 16:15:32 -0300

    add corpo site

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,25 @@ p10.addEventListener('click', fp10 );
 function fp10(){
     p10.style.display = 'none';
 } // alterar "p1 e fp1"
+
+
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150; 
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < (offset + height)){
+            navLinks.forEach(links => {
+                links.classList.remove('ativa');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('ativa');
+
+            });
+        };
+
+    });
+}
